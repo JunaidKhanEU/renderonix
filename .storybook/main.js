17132198170279cbc6f18b482d7224f79f029b56
Junaid Khan <37302875+JunaidKhanEU@users.noreply.github.com>
2021-07-26 13:24:09 +0100

    Develop to Master (#1)
    * storybook hosting done
    
    * bug fix
    
    Co-authored-by: junaidkhan-aardy <junaid@aardy.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  refs: {
+    "chromatic-published-Storybook": {
+      // The title of your Storybook
+      title: "Renderonix",
+      // The url provided by Chromatic when it was published
+      url: "https://renderonix.chromatic.com",
+    },
+  },
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+    "@storybook/preset-create-react-app",
+  ],
 }
